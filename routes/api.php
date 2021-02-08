f4c5e072919333a131746c12b32f216a94259583
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 17:54:34 -0300

    new: route to retrieve all events

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('events')->group(function () {
 
     Route::post('/', [EventsController::class, 'create']);
+    Route::get('/', [EventsController::class, 'list']);
     Route::get('/{id}', [EventsController::class, 'retrieve']);
     Route::put('/{event}', [EventsController::class, 'update']);
     Route::delete('/{event}', [EventsController::class, 'delete']);
