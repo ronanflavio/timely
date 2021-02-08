0c6ba2a9f19f34bd6b21345d8187f1f120c51847
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 08:38:34 -0300

    new: event's controller

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataTransferObjects\Event\CreateEventData;
+
+class EventsController extends Controller
+{
+    public function create()
+    {
+        //
+    }
+
+    public function retrieve()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+}
