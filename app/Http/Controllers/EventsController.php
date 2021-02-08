9ba553ed794b132db787a7c839fdcb404f011aea
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 09:17:14 -0300

    new: defining controller action that will create a new event

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Event\CreateEventAction;
 use App\DataTransferObjects\Event\CreateEventData;
+use App\Http\Requests\Event\CreateEventRequest;
 
 class EventsController extends Controller
 {
-    public function create()
+    public function create(CreateEventRequest $request, CreateEventAction $action)
     {
-        //
+        $data = CreateEventData::fromRequest($request);
+        $response = $action($data);
+        return response()->json($response, 201);
     }
 
     public function retrieve()
