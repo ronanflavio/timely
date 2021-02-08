f32db41378829f34cd894cdefa8177390a928c8d
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 09:33:45 -0300

    new: defining route to retrieve an event

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('events')->group(function () {
 
     Route::post('/', [EventsController::class, 'create']);
+    Route::get('/{id}/retrieve', [EventsController::class, 'retrieve']);
 
 });
