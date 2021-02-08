5bb8172e5b13b8601e98d26b9fe3b374b6a958df
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 11:52:03 -0300

    new: validating if organizers do exists before save. If one or more organizers do not exist, the transaction have to rollback

--- app/Actions/Event/CreateEventAction.php
+++ app/Actions/Event/CreateEventAction.php
@@ -3,26 +3,45 @@
 namespace App\Actions\Event;
 
 use App\DataTransferObjects\Event\CreateEventData;
+use App\Exceptions\Organizer\OneOrMoreOrganizersDoNotExistException;
 use App\Models\Event;
+use App\Models\Organizer;
+use Illuminate\Support\Facades\DB;
 
 class CreateEventAction
 {
-    public function __invoke(CreateEventData $data): int
+    public function __invoke(CreateEventData $data, Event $event = null): int
     {
-        $event = new Event();
-        $event->title = $data->title;
-        $event->description = $data->description;
-        $event->start_datetime = $data->start_datetime;
-        $event->end_datetime = $data->end_datetime;
-        $event->save();
+        DB::beginTransaction();
 
-        $this->attachOrganizers($event, $data->organizers);
+        try {
+            $event = $event ? $event : new Event();
+            $event->title = $data->title;
+            $event->description = $data->description;
+            $event->start_datetime = $data->start_datetime;
+            $event->end_datetime = $data->end_datetime;
+            $event->save();
 
-        return $event->id;
+            $this->synchOrganizers($event, $data->organizers);
+            DB::commit();
+            return $event->id;
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
     }
 
-    private function attachOrganizers(Event $event, array $organizers): void
+    private function synchOrganizers(Event $event, array $organizers): void
     {
-        $event->organizers()->attach($organizers);
+        if (!$this->organizersExist($organizers)) {
+            throw new OneOrMoreOrganizersDoNotExistException();
+        }
+        $event->organizers()->sync($organizers);
+    }
+
+    private function organizersExist(array $organizers)
+    {
+        $organizersCollection = Organizer::find($organizers);
+        return $organizersCollection->count() === sizeof($organizers);
     }
 }
