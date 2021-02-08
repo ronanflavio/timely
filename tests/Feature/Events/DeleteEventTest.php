90b7981ffc09aa6e74e80b1d7d3a673afcf6d865
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 10:38:03 -0300

    new: coverage test case to delete event

---
+++ tests/Feature/Events/DeleteEventTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Events;
+
+use App\Models\Event;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DeleteEventTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+        Event::factory()->create();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldDeleteEventWhenIdExistis()
+    {
+        $id = 1;
+
+        $response = $this->json(
+            'DELETE',
+            '/api/events/' . $id,
+        );
+
+        $response->assertStatus(201);
+    }
+}
