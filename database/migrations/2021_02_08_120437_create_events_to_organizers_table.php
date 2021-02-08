106b5df4efab0c202e972498785273965b1924f0
Ronan Flavio <ronan.flavio@hotmail.com>
2021-02-08 09:18:49 -0300

    new: creating a migration the allows many to many relation with events and organizers

---
+++ database/migrations/2021_02_08_120437_create_events_to_organizers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsToOrganizersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events_to_organizers', function (Blueprint $table) {
+            $table->integer('event_id');
+            $table->integer('organizer_id');
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->foreign('organizer_id')->references('id')->on('organizers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events_to_organizers');
+    }
+}
